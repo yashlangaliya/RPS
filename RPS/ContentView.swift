cceae62661fae0b0737ee7cf5015bd6f3b16041f
yashlangaliya <l.yash@ivymobility.com>
2020-02-04 16:51:52 +0530

    Initial Commit

---
+++ RPS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  RPS
+//
+//  Created by stl-037 on 04/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
